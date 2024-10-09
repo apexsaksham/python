ff51f5a76f25c49798ab918677b0418a73aa398e
apexsaksham <saksham26singh@gmail.com>
2024-10-09 17:06:54 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,51 @@
+print("hello saksham ")
+
+def chai(n):
+    
+   print(n)
+
+#chai("lemon tea")   
+
+#if 5 > 2:
+ # print("Five is greater than two!")
+
+#if 5 > 4:
+ #  print("5 is greater number")
+
+
+# x = 6
+# x = "lodu"
+# print(x)
+
+# x = 5
+# y = "John"
+# print(type(x))
+# print(type(y))
+
+
+# import calc
+
+# print(calc.add(10, 2))
+
+from math import sqrt, factorial
+
+print(sqrt(4))
+print(factorial(5))
+
+import datetime
+from datetime import date
+import time
+from datetime import datetime
+# print(time.time())
+currenttime = datetime.now()
+print(currenttime)
+
+
+import emoji
+
+print(emoji.emojize(" :skull:"))
+print(emoji.emojize(" :face_savoring_food:"))
+print(emoji.emojize(" :exploding_head:"))
+print(emoji.emojize(" :clown_face:"))
+
+
